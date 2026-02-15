b15bca16f41e577f74751021bdeecc2f69b20600
Akigaii <reece.galgana@gmail.com>
2026-02-15 13:54:47 -0800

    added 80 gate covereage enhancement for ci pipeline

--- ci_lab/tests/test_counter.py
+++ ci_lab/tests/test_counter.py
@@ -277,3 +277,5 @@ class TestCounterEndpoints:
         assert response.status_code == HTTPStatus.BAD_REQUEST
 
         # TODO: Add an assertion to verify the error message specifically says 'Invalid counter name'S
+
+
